e2fe8563dafca448f4792960a6247f7cb47bf26a
Sedki-Sghairi <sedkisghairi3@gmail.com>
2020-12-28 10:57:30 +0400

    custom react-native styled components

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import Card from '../components/Card';
 export default function StartGameScreen(props) {
 	return (
 		<View style={styles.screen}>
 			<Text style={styles.title}>Start A New Game!</Text>
-			<View style={styles.inputContainer}>
+			<Card style={styles.inputContainer}>
 				<Text>Select A Number</Text>
 				<TextInput />
 				<View style={styles.buttonContainer}>
 					<Button title="reset" onPress={() => {}} />
 					<Button title="confirm" onPress={() => {}} />
 				</View>
-			</View>
+			</Card>
 		</View>
 	);
 }
@@ -30,19 +31,7 @@ const styles = StyleSheet.create({
 	inputContainer: {
 		width: 300,
 		maxWidth: '80%',
-		alignItems: 'center',
-		borderRadius: 5,
-		shadowColor: '#000',
-		shadowOffset: {
-			width: 0,
-			height: 2
-		},
-		shadowOpacity: 0.26,
-		shadowRadius: 6,
-		backgroundColor: '#fff',
-		padding: 10,
-		elevation: 5,
-		borderRadius: 5
+		alignItems: 'center'
 	},
 	title: {
 		fontSize: 20,
