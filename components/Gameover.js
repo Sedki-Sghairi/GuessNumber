c38776e8120457892e62e45e6051e79b6b3ae21a
Sedki-Sghairi <sedkisghairi3@gmail.com>
2020-12-29 22:45:57 +0400

    add  image locally

--- components/Gameover.js
+++ components/Gameover.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
+import { colors } from '../constants/colors';
 
 export default function Gameover(props) {
 	return (
 		<View style={styles.screen}>
-			<Text>game over</Text>
-			<Text>Number of Rounds: {props.rounds}</Text>
+			<Text style={styles.gameover}>game over</Text>
+			<Image style={styles.image} source={require('../assets/success.png')} />
+			<Text style={styles.marginized}>Number of Rounds: {props.rounds}</Text>
 			<Button title="New Game" onPress={props.restartNewGame} />
 		</View>
 	);
@@ -16,5 +18,17 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center'
+	},
+	gameover: {
+		color: colors.orange,
+		fontWeight: 'bold'
+	},
+	marginized: {
+		marginVertical: 10
+	},
+	image: {
+		width: '80%',
+		height: 300,
+		borderRadius: 10
 	}
 });
